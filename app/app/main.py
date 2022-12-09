2a1372e8891dccfeb288b6bd61c7e0504c4efd95
e-kondr01 <e.kondr01@gmail.com>
2022-12-09 11:26:34 +0300

    feat: return cors

--- app/app/main.py
+++ app/app/main.py
@@ -11,13 +11,13 @@ app = FastAPI(
     redoc_url="/api/redoc",
 )
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api")
 
