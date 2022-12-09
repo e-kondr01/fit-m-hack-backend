4f5ca5eef53d89b90839076b951fa0006dbe5d6d
e-kondr01 <e.kondr01@gmail.com>
2022-12-09 14:53:21 +0300

    feat: add users list

--- app/app/api/api.py
+++ app/app/api/api.py
@@ -1,4 +1,4 @@
-from app.api.endpoints import article, auth, feature, quiz
+from app.api.endpoints import article, auth, feature, quiz, user
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -24,4 +24,10 @@ api_router.include_router(
     article.router,
     prefix="/articles",
     tags=["Статьи"],
+)
+
+api_router.include_router(
+    user.router,
+    prefix="/users",
+    tags=["Пользователи"],
 )
