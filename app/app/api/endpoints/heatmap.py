30c69067c48af56a7ebfc0f24a5e44703f51b559
e-kondr01 <e.kondr01@gmail.com>
2022-12-09 15:37:18 +0300

    fix: date

--- app/app/api/endpoints/heatmap.py
+++ app/app/api/endpoints/heatmap.py
@@ -45,7 +45,7 @@ async def get_quiz_heatmap(
 
     if quizzes:
         first_quiz = quizzes[0]
-        columns.append(str(first_quiz.created_at).split("T")[0])
+        columns.append(str(first_quiz.created_at).split(" ")[0])
         date_data = []
         for question in first_quiz.completed_questions:
             if question.type == QuestionTypes.RANGE:
@@ -60,7 +60,7 @@ async def get_quiz_heatmap(
 
         for quiz in quizzes:
             date_data = []
-            columns.append(cast(quiz.created_at, Date))
+            columns.append(str(quiz.created_at).split(" ")[0])
             for question in quiz.completed_questions:
                 if question.type == QuestionTypes.RANGE:
                     norm_answer = normalize(
